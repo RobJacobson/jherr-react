92e25be8d682ce24d8b4747e89dde32b58205b01
Rob Jacobson <rljacobson@outlook.com>
2024-09-02 22:36:54 -0700

    Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => (
+  <div
+    style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem",
+    }}
+  >
+    <h1 className="title">Pokemon Search</h1>
+    <table width="100%">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bulbasaur</td>
+          <td>Grass, Poison</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
 
 export default App;
